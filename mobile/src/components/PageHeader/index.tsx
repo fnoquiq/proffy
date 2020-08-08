c3ba937aaa39736e0625bf80babb611eef603930
Gabriel Mesquita <gabrielteixeiramesquita@gmail.com>
2020-08-08 15:16:38 -0300

    :rocket: A milha extra
    Dia 5

--- mobile/src/components/PageHeader/index.tsx
+++ mobile/src/components/PageHeader/index.tsx
@@ -1,19 +1,20 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import { View, Image, Text } from 'react-native';
 import { BorderlessButton } from 'react-native-gesture-handler';
+import { useNavigation } from '@react-navigation/native';
 
 import backIcon from '../../assets/images/icons/back.png';
 import logoImg from '../../assets/images/logo.png';
 
 import styles from './styles';
-import { useNavigation } from '@react-navigation/native';
 
 interface PageHeaderProps {
   title: string;
+  headerRight?: ReactNode;
+}
 
-};
+const PageHeader: React.FC<PageHeaderProps> = ({ title, children, headerRight }) => {
 
-const PageHeader: React.FC<PageHeaderProps> = ({ title }) => {
   const { navigate } = useNavigation();
 
   function handleGoBack() {
@@ -30,9 +31,14 @@ const PageHeader: React.FC<PageHeaderProps> = ({ title }) => {
         <Image source={logoImg} resizeMode="contain" />
       </View>
 
-      <Text style={styles.title}>{title}</Text>
+      <View style={styles.header}>
+        <Text style={styles.title}>{title}</Text>
+        {headerRight}
+      </View>
+
+      {children}
     </View>
-  )
-};
+  );
+}
 
 export default PageHeader;
--- mobile/src/routes/StudyTabs.tsx
+++ mobile/src/routes/StudyTabs.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Ionicons } from '@expo/vector-icons'
+import { Ionicons } from '@expo/vector-icons';
 
 import TeacherList from '../pages/TeacherList';
 import Favorites from '../pages/Favorites';
